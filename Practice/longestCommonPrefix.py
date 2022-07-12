7c57d1a9c706e50b1af2e3da05ad119a2f8f8cfd
Nefil <48553218+xXNefilXx@users.noreply.github.com>
2022-07-12 09:20:37 +0545

    Update longestCommonPrefix.py

--- Practice/longestCommonPrefix.py
+++ Practice/longestCommonPrefix.py
@@ -1,16 +1,30 @@
 strs=["flower","flow","flight"]
+strs2=["flow","flower"]
 
 def longestCommonPrefix(strs):
-        a=min(len(strs[0]),len(strs[1]), len(strs[2]))
+        a=len(min(strs,key=len))
+        b=len(strs)
+        if b==1:
+            return strs[0]
         k=[' ']*a
-        i=0
-        while i < a:
-            if strs[0][i]==strs[1][i] and strs[1][i]==strs[2][i]:
-                k[i]=strs[0][i]
-            i=i+1
+        
+        j=0
+        while j < a:
+            i=0
+            while i < b-1:
+                if strs[i][j]==strs[i+1][j]:
+                    i=i+1
+                    if i==b-1:
+                        k[j]=strs[i][j]
+                    continue
+                else:
+                    j=a
+                    break
+            j=j+1
         if k==[' ']*a:
             return ""
         else:
             r= (''.join(k))
             m= r.split()
             return m[0]
+print(longestCommonPrefix(strs2))
