a524c84059a56c89c8960014d75c6e88ec7196cd
Nefil <48553218+xXNefilXx@users.noreply.github.com>
2022-08-24 11:32:21 +0545

    Create addtwonumberslist.py

---
+++ Practice/addtwonumberslist.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        addends = l1, l2
+        dummy = end = ListNode(0)
+        carry = 0
+        while addends or carry:
+            carry += sum(a.val for a in addends)
+            print(carry)
+            addends = [a.next for a in addends if a.next]
+            end.next = end = ListNode(carry % 10)
+            carry /= 10
+        return dummy.next
